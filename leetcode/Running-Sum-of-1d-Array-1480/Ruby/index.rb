883184efb0103e31a10467e7b255e114d9fc1c37
Dennis Njogu <Dcolonel6@users.noreply.github.com>
2023-04-22 13:02:56 +0300

    added my performance details

--- leetcode/Running-Sum-of-1d-Array-1480/Ruby/index.rb
+++ leetcode/Running-Sum-of-1d-Array-1480/Ruby/index.rb
@@ -1,5 +1,6 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
+#performance time: 90ms, memory 211MB
 def running_sum(nums)
     nums.each_with_object([]) {|ele,obj| obj << obj.fetch(-1, 0) + ele}
 end
