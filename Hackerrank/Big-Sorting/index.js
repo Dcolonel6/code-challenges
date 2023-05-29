bc9d462c168da8e661fbab88c71beb278a96649f
Dennis Njogu <Dcolonel6@users.noreply.github.com>
2023-05-30 00:39:38 +0300

    added support for large numbers

--- Hackerrank/Big-Sorting/index.js
+++ Hackerrank/Big-Sorting/index.js
@@ -5,7 +5,7 @@ function bigSorting(unsorted) {
     }
     pivot_index = Math.floor(Math.random() * unsorted.length);
     pivot = unsorted[pivot_index];
-    less = unsorted.filter((ele, index) => pivot_index !== index && +ele <= +pivot)
-    more = unsorted.filter((ele, index) => pivot_index !== index && +ele > +pivot)
+    less = unsorted.filter((ele, index) => pivot_index !== index && BigInt(ele) <= BigInt(pivot))
+    more = unsorted.filter((ele, index) => pivot_index !== index && BigInt(ele) > BigInt(pivot))
     return [...bigSorting(less), pivot, ...bigSorting(more)]
 }
