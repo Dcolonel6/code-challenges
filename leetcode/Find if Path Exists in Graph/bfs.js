0b50867723a1da4a0a79c5f1780977eac29abf71
Dennis Njogu <d.n_mwangi@outlook.com>
2023-06-02 23:33:59 +0300

    :recycle: refactored code to be more modular

--- leetcode/Find if Path Exists in Graph/bfs.js
+++ leetcode/Find if Path Exists in Graph/bfs.js
@@ -1,3 +1,4 @@
+const {buildGraph} = require("./graph");
 /**
  * @param {number} n
  * @param {number[][]} edges
@@ -5,6 +6,7 @@
  * @param {number} destination
  * @return {boolean}
  */
+
 var validPath = function(n, edges, source, destination) {
     const graph = buildGraph(edges);
     const hasPath = (grph, src, dest) => {
@@ -24,17 +26,4 @@ var validPath = function(n, edges, source, destination) {
     
 };
 
-const buildGraph = (edges) => {
-    const graph = {};
-    for(let edge of edges){
-        const [vertixA, vertixB] = edge
-        //checks to see if vertix in graph if not its initialize an empty ary
-        if(!(vertixA in graph)) graph[vertixA] = []
-        if(!(vertixB in graph)) graph[vertixB] = []
-        graph[vertixA].push(vertixB)
-        graph[vertixB].push(vertixA)
-    }
-
-    return graph
-};
-console.log(validPath(6, [[0,1],[0,2],[3,5],[5,4],[4,3]], 0, 5))
+const {buildGraph} = require("./graph");
