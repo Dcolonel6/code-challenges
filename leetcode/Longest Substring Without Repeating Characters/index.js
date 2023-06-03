f7ecf5d45bce1b91739a7d9054ae3d02c3853add
Dennis Njogu <d.n_mwangi@outlook.com>
2023-06-03 19:08:37 +0300

    :bug: handled an edge case

--- leetcode/Longest Substring Without Repeating Characters/index.js
+++ leetcode/Longest Substring Without Repeating Characters/index.js
@@ -3,7 +3,8 @@ const bfs = require('./bfs')
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {    
+var lengthOfLongestSubstring = function(s) { 
+    if(s.length <= 1) return s.length   
     let count = 0
     let str = s.slice(count)
     while(str.length){             
@@ -20,3 +21,4 @@ console.log(lengthOfLongestSubstring("abcabcbb"))
 console.log(lengthOfLongestSubstring("bbbbb"))
 console.log(lengthOfLongestSubstring("pwwkew"))
 console.log(lengthOfLongestSubstring("abcdefgh"))
+console.log(lengthOfLongestSubstring(""))
