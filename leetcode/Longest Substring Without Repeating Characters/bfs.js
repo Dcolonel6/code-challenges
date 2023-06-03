818c9f6543a95e69145a21d42d53985a47a1fc02
Dennis Njogu <d.n_mwangi@outlook.com>
2023-06-03 19:01:29 +0300

    :bug: fixed bug

--- leetcode/Longest Substring Without Repeating Characters/bfs.js
+++ leetcode/Longest Substring Without Repeating Characters/bfs.js
@@ -2,21 +2,30 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function(s) {    
+    let count = 0
+    let str = s.slice(count)
+    while(str.length){             
+        const subString = bfs(str)        
+        if(s.includes(subString)) return subString.length
+        count++
+        str = s.slice(count)
+    }
+};
+
+const bfs = (s) => {
     const queue = s.split("")
     const alreadyVisited = new Set()
-    //let longest = ""
-
     while(queue.length){
         const current = queue.shift()
         if(!alreadyVisited.has(current)){
             alreadyVisited.add(current)
-        }
-        else {
-            return Array.from(alreadyVisited).join('')
-        }
+        }  
     }
-   return Array.from(alreadyVisited).join('') 
-};
+   return Array.from(alreadyVisited).join('')
+}
 
 console.log(lengthOfLongestSubstring("abcabcbb"))
+console.log(lengthOfLongestSubstring("bbbbb"))
+console.log(lengthOfLongestSubstring("pwwkew"))
+console.log(lengthOfLongestSubstring("abcdefgh"))
