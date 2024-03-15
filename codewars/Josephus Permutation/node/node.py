d8dbc1ed5cda8b3a38a9df8ff6a16195cd8e645b
Dennis M Njogu <dcolonel6@gmail.com>
2024-03-15 18:15:59 +0300

    converts sequences to circular linked nodes

--- codewars/Josephus Permutation/node/node.py
+++ codewars/Josephus Permutation/node/node.py
@@ -9,3 +9,24 @@ class Node():
         return f"{self.val}"
 
 
+def make_circular_linked_list(items):
+    start_node = None
+    last_node = None
+    prev_node = None
+    for index, item in enumerate(items):
+        n = Node(item, index)
+        if index == 0:
+            start_node = n
+        if index == len(items) - 1:
+            last_node = n
+            n.next = start_node
+            start_node.prev = n
+        n.prev = prev_node
+        try:
+            prev_node.next = n
+        except AttributeError:
+            print(f"try setting {prev_node} next")
+        prev_node = n
+    start_node.prev = last_node
+
+    return start_node
