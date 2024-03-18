028c48676a59e43594c88e9a691c18659b95e494
Dennis M Njogu <dcolonel6@gmail.com>
2024-03-18 18:53:14 +0300

    test for accuracy

---
+++ codewars/valid braces/python/test.py
@@ -0,0 +1,19 @@
+import unittest
+from main import valid_braces
+
+class TestValidBraces(unittest.TestCase):
+
+    def test_valid(self):
+        self.assertTrue(valid_braces("()"))
+        self.assertTrue(valid_braces("{}" ))
+        self.assertTrue(valid_braces("{}()[]") )
+        self.assertTrue(valid_braces("([{}])"))
+        self.assertTrue(valid_braces("[]"))
+
+    def test_invalid(self):
+        self.assertFalse(valid_braces("(}"))
+        self.assertFalse(valid_braces("[(])"))
+
+
+if __name__ == '__main__':
+    unittest.main()
