52e286c5d5dd8da93ff74f1d2b2746d53b509eac
Dennis M Njogu <dcolonel6@gmail.com>
2024-03-18 21:55:53 +0300

    solution in js

---
+++ codewars/valid braces/javascript/index.js
@@ -0,0 +1,24 @@
+var isValid = function(s) {   
+    const stack = [];
+    const aryCharacters = s.split("");    
+    
+    for (const character of aryCharacters) {
+        if (character === "(" || character === "{" || character === "[") {
+        stack.push(character);
+        } else if (character === ")" && stack[stack.length -1] === "(") {
+            stack.pop();      
+        } else if (character === "}" && stack[stack.length -1] === "{") {
+            stack.pop();
+        } else if (character === "]" && stack[stack.length -1] === "[") {
+            stack.pop();
+        }else{
+            return false;  
+        } 
+    }  
+    return stack.length <= 0;
+};
+
+const possibilitiesOfS = ["()", "{[]}", "()[]{}", "(]", "[", "((" ];
+possibilitiesOfS.forEach((possible) => {
+  console.log(`${possible} =>`, isValid(possible));
+});
