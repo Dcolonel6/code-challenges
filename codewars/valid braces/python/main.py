5a35ef8d9d1aeaca4e041b6acc2db1eb58e39191
Dennis M Njogu <dcolonel6@gmail.com>
2024-03-18 21:57:57 +0300

    wrap 'pop' in a try/except incase of empty stack

--- codewars/valid braces/python/main.py
+++ codewars/valid braces/python/main.py
@@ -2,11 +2,15 @@ def valid_braces(string):
     stack = []
     closes_to_open = ("]", ")", "}")
 
+
     for brace in string:
         # check if brace is a closing one
         if brace in closes_to_open:
             # brace has an equivalent closing brace in the stack
-            closing_brace = stack.pop()
+            try:
+                closing_brace = stack.pop()
+            except IndexError:
+                return False
             if brace is not closing_brace:
                 return False        
              
@@ -17,6 +21,7 @@ def valid_braces(string):
                 stack.append("}")
             if brace == "[":
                 stack.append("]")
+
     return True
 
 
