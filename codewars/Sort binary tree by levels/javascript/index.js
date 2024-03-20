a8092e5e6b32258c1bcb75c5988815cfab03b3d0
Dennis M Njogu <dcolonel6@gmail.com>
2024-03-20 16:33:40 +0300

    make condition more concise

--- codewars/Sort binary tree by levels/javascript/index.js
+++ codewars/Sort binary tree by levels/javascript/index.js
@@ -2,7 +2,7 @@ function treeByLevels (rootNode) {
     if(rootNode === null)	return [];
     let queue = [rootNode]
     let values = []
-    while (queue.length > 0){
+    while (queue.length){
       let current = queue.shift()
       values = [...values, current.value]
       if( current.left !== null) queue = [...queue, current.left]
