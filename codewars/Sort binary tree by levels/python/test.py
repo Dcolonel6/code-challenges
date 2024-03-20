db334dfab9704a1a94fdc4949e87213f67daf847
Dennis M Njogu <dcolonel6@gmail.com>
2024-03-20 18:38:37 +0300

    test node creation

--- codewars/Sort binary tree by levels/python/test.py
+++ codewars/Sort binary tree by levels/python/test.py
@@ -6,3 +6,11 @@ class TestNode(unittest.TestCase):
     def setUp(self) -> None:
         self.node = Node(2, 3, 10)
 
+    def test_node_creation(self):
+        self.assertEqual(self.node.left, 2) 
+        self.assertEqual(self.node.right, 3)  
+        self.assertEqual(self.node.value, 10)     
+        self.assertIsInstance(self.node, Node)
+
+
+
