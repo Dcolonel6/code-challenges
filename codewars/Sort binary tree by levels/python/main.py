11f433d0e6e67d0ed142cd38bd823aaa6b14dab0
Dennis M Njogu <dcolonel6@gmail.com>
2024-03-20 17:12:26 +0300

    add examples

--- codewars/Sort binary tree by levels/python/main.py
+++ codewars/Sort binary tree by levels/python/main.py
@@ -1,3 +1,10 @@
+class Node:
+    def __init__(self, L=None, R = None, n =None):
+        self.left = L
+        self.right = R
+        self.value = n
+
+
 def tree_by_levels(node):
     if node is None:
         return []
@@ -7,7 +14,7 @@ def tree_by_levels(node):
     while len(queue):
         try:
             current = queue.pop(0)
-            values.append(current.value)
+            values = [*values, current.value]
             if current.left is not None:
                 queue = [*queue, current.left]
             if current.right is not None:
@@ -16,3 +23,6 @@ def tree_by_levels(node):
                 pass
     
     return values
+
+print(tree_by_levels(None), [])
+print(tree_by_levels(Node(Node(None, Node(None, None, 4), 2), Node(Node(None, None, 5), Node(None, None, 6), 3), 1)))
