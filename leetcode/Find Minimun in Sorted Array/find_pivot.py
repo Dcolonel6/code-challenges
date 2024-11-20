2ba10b4f2c933ab9634280b494cddc98c2b1e4da
dcolonel6 <dcolonel6@gmail.com>
2024-11-20 23:46:05 +0300

    Solved with time complexity of O(log n)

---
+++ leetcode/Find Minimun in Sorted Array/find_pivot.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def find_pivot(self, nums:List[int]) -> List[int]:
+        n = len(nums)
+        low_index = 0
+        high_index = n-1
+
+        while low_index < high_index:
+            mid = (low_index + high_index) // 2
+            mid_value, high_value = nums[mid], nums[high_index]
+
+            if mid_value > high_value:
+                #adjust the low to be midindex +1
+                low_index = mid + 1
+            else:
+                high_index = mid
+        return [low_index, high_index]
+
+if __name__ == "__main__":
+    s = Solution().find_pivot([7,8,9,1,2,3,4,5,6])
+    print(s)
