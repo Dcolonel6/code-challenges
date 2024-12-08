47265ec89789c1708fd13148a64c5abdff763d9e
Dennis Njogu <dcolonel6@gmail.com>
2024-12-08 22:04:31 +0300

    runtime o(n)

--- leetcode/Merge  Strings Alternately/merge_strings_alternately.py
+++ leetcode/Merge  Strings Alternately/merge_strings_alternately.py
@@ -1,3 +1,18 @@
+from itertools import zip_longest
+
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        pass
+        combined_string = ""
+
+        for (x, y) in  zip_longest(word1, word2, fillvalue="*"):
+            if x.isalpha():
+                combined_string += x
+            if y.isalpha():
+                combined_string += y
+        return combined_string
+
+
+
+
+if __name__ == "__main__":
+    print(Solution().mergeAlternately("ab", "pqrs"))
