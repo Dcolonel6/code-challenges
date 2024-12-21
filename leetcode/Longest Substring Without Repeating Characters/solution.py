11d7c3686cf4f68a30288920d1f70df87e9acedd
Dennis Njogu <dcolonel6@gmail.com>
2024-12-21 12:20:43 +0300

    O(n) solution for minimum size sub array sum

---
+++ leetcode/Longest Substring Without Repeating Characters/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        length = len(s)
+        tracker = {}
+        left = 0
+        index = 0
+        max_length = 0
+        substr = []
+
+        while index < length:
+            letter = s[index]
+            if letter in tracker:
+                # if it exist, then we have encountered a duplicate, move left pointer
+                if tracker[letter] >= left:
+                    left = tracker[letter] + 1
+            tracker[letter] = index
+            substr.append(s[left: index+1])
+            # always compares our previous window with the currrent one, returns the largest
+            max_length = max(max_length, (index - left) + 1)
+            index += 1
+        print(substr)
+        return max_length
+
+
+if __name__ == "__main__":
+    print(Solution().lengthOfLongestSubstring("abcabcbb"))
+    print(Solution().lengthOfLongestSubstring("bbbbb"))
+    print(Solution().lengthOfLongestSubstring("pwwkew"))
+    print(Solution().lengthOfLongestSubstring("aab"))
