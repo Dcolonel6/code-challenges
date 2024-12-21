cf7670101b27b4186b8c9eb00346db5e6cf5b675
Dennis Njogu <dcolonel6@gmail.com>
2024-12-21 12:18:53 +0300

    unit tests for minimum size sub array sum

---
+++ leetcode/Longest Substring Without Repeating Characters/test_solution.py
@@ -0,0 +1,24 @@
+import unittest
+from solution import Solution
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.longest_substr = Solution().lengthOfLongestSubstring
+
+    def test_case1(self):
+        self.assertEqual(self.longest_substr("abcabcbb"), 3)  # add assertion here
+
+    def test_case2(self):
+        self.assertEqual(self.longest_substr("bbbbb"), 1)  # add assertion here
+
+    def test_case3(self):
+        self.assertEqual(self.longest_substr("pwwkew"), 3)  # add assertion here
+
+    def test_case4(self):
+        self.assertEqual(self.longest_substr("aab"), 2)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
