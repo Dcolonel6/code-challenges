4dd8db8719aa522cd0857d6f028fc796359492da
Dennis Njogu <dcolonel6@gmail.com>
2025-01-09 20:07:48 +0300

    added explanatory comments

--- leetcode/Product of Array Except Self (238)/product_array_except_self.py
+++ leetcode/Product of Array Except Self (238)/product_array_except_self.py
@@ -10,12 +10,15 @@ class Solution:
         ans = [1] * length
         running_product = 1
 
+        # get the product to the left of the current element
         for index, num in enumerate(nums):
             left_product_ary[index] = running_product
             running_product *= num
 
         running_product = 1
 
+        # get the product to the right of the current element  in nums and then multiply left_ary[i] * right[n-i-1]
+        # basically the reverse
         for idx, num in enumerate(nums[::-1]):
             ans[idx] = running_product * left_product_ary[length - 1 - idx]
             running_product *= num
