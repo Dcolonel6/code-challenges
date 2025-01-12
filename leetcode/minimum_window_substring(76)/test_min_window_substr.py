26e67401ee58b73f08dd37f64f7c9bf0aa716c58
Dennis Njogu <dcolonel6@gmail.com>
2025-01-13 00:50:45 +0300

    added test with a single edge case

--- leetcode/minimum_window_substring(76)/test_min_window_substr.py
+++ leetcode/minimum_window_substring(76)/test_min_window_substr.py
@@ -1,9 +1,17 @@
 import unittest
+from min_window_substr import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.min_window = Solution().minimum_window
+
+    def test_case1(self):
+        self.assertEqual(self.min_window("ADOBECODEBANC", "ABC"), 4)  # add assertion here
+
+    def test_with_no_substring(self):
+        self.assertEqual(self.min_window("a", "aa"), "")  # add assertion here
 
 
 if __name__ == '__main__':
