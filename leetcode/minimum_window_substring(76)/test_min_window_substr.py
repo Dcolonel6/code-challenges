f175306ff0bb0937eed5f175964de3a0cb4be9c3
Dennis Njogu <dcolonel6@gmail.com>
2025-01-15 22:17:16 +0300

    added extra edge cases

--- leetcode/minimum_window_substring(76)/test_min_window_substr.py
+++ leetcode/minimum_window_substring(76)/test_min_window_substr.py
@@ -8,7 +8,7 @@ class MyTestCase(unittest.TestCase):
         self.min_window = Solution().minimum_window
 
     def test_case1(self):
-        self.assertEqual(self.min_window("ADOBECODEBANC", "ABC"), 4)  # add assertion here
+        self.assertEqual(self.min_window("ADOBECODEBANC", "ABC"), "BANC")  # add assertion here
 
     def test_with_no_substring(self):
         self.assertEqual(self.min_window("a", "aa"), "")  # add assertion here
