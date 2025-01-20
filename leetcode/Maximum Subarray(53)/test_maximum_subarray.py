1b2be82a4b0c1c30becb0d6a2a6cda661a528bf6
Dennis Njogu <dcolonel6@gmail.com>
2025-01-20 23:42:15 +0300

    added edge test cases

--- leetcode/Maximum Subarray(53)/test_maximum_subarray.py
+++ leetcode/Maximum Subarray(53)/test_maximum_subarray.py
@@ -1,9 +1,20 @@
 import unittest
+from maximum_subarray import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.max_subarry = Solution().maximum_subarray
+
+    def test_case1(self):
+        self.assertEqual(self.max_subarry([-2,1,-3,4,-1,2,1,-5,4]), 6)  # add assertion here
+
+    def test_case2(self):
+        self.assertEqual(self.max_subarry([1]), 1)  # add assertion here
+
+    def test_case3(self):
+        self.assertEqual(self.max_subarry([5,4,-1,7,8]), 23)  # add assertion here
 
 
 if __name__ == '__main__':
