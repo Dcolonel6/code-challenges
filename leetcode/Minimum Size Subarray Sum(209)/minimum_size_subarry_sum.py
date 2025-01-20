7f04d17c0c50559eacb5ed6185a57d928da485ad
Dennis Njogu <dcolonel6@gmail.com>
2025-01-21 00:24:29 +0300

    added problem statement

--- leetcode/Minimum Size Subarray Sum(209)/minimum_size_subarry_sum.py
+++ leetcode/Minimum Size Subarray Sum(209)/minimum_size_subarry_sum.py
@@ -1,5 +1,10 @@
 from typing import List
 
+"""
+    Given an array of positive integers nums and a positive integer target, return the minimal length of a
+    subarray
+    whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+"""
 
 class Solution:
 
