a668774970186adf04ed911891b53cebc83706ef
Dennis Njogu <dcolonel6@gmail.com>
2025-01-20 23:41:50 +0300

    runtime 0(n) spacetime 0(1) used kadane's algorithm to solve the question

---
+++ leetcode/Maximum Subarray(53)/maximum_subarray.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+"""
+    Given an integer array nums, find the
+    subarray
+    with the largest sum, and return its sum.
+"""
+class Solution:
+
+    def maximum_subarray(self, nums: List[int]) -> int:
+        current_sum = 0
+        max_sum = float('-inf')
+        left = 0
+
+        for idx, num in enumerate(nums):
+            current_sum += num
+            max_sum = max(current_sum, max_sum)
+            if current_sum < 0:
+                current_sum = 0
+                left = idx
+
+        return max_sum
+
+
+if __name__ == "__main__":
+    print(Solution().maximum_subarray([5,4,-1,7,8]))
