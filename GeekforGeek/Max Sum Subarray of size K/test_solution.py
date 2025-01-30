2757239e3db1f7278f9ca66486edb1d3065aa8c1
dcolonel6 <dcolonel6@gmail.com>
2025-01-30 12:44:24 +0300

    added test cases

--- GeekforGeek/Max Sum Subarray of size K/test_solution.py
+++ GeekforGeek/Max Sum Subarray of size K/test_solution.py
@@ -1,9 +1,22 @@
 import unittest
 
+from solution import Solution
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.max_sum_subary = Solution().maximum_sum_subarray
+
+    def test_case1(self):
+        self.assertEqual(self.max_sum_subary([100, 200, 300, 400], 2), 700)  # add assertion here
+
+    def test_case2(self):
+        self.assertEqual(self.max_sum_subary([100, 200, 300, 400], 4), 1000)  # add assertion here
+
+    def test_case3(self):
+        self.assertEqual(self.max_sum_subary([100, 200, 300, 400], 1), 400)  # add assertion here
+
 
 
 if __name__ == '__main__':
