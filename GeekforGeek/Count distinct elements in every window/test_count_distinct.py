9e4dc62c080303ac5120d923f7a0da4ac9ace28c
dcolonel6 <dcolonel6@gmail.com>
2025-01-30 12:16:21 +0300

    added test cases

--- GeekforGeek/Count distinct elements in every window/test_count_distinct.py
+++ GeekforGeek/Count distinct elements in every window/test_count_distinct.py
@@ -1,9 +1,18 @@
 import unittest
+from count_distinct import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self):
+        self.count_distinct = Solution().count_distinct
+    def test_case1(self):
+        self.assertListEqual(self.count_distinct([1, 2, 1, 3, 4, 2, 3], 4), [3, 4, 4, 3])  # add assertion here
+
+    def test_case2(self):
+        self.assertListEqual(self.count_distinct([4,1,1], 2), [2, 1])  # add assertion here
+
+    def test_case3(self):
+        self.assertListEqual(self.count_distinct( [1, 1, 1, 1, 1], 3), [1, 1, 1])  # add assertion here
 
 
 if __name__ == '__main__':
