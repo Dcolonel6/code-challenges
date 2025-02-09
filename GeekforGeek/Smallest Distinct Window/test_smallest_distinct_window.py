2a0e6e404452fd7728ccba87cad434fbf068f604
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-09 16:04:17 +0300

    Update test_smallest_distinct_window.py
    added edge testcases

--- GeekforGeek/Smallest Distinct Window/test_smallest_distinct_window.py
+++ GeekforGeek/Smallest Distinct Window/test_smallest_distinct_window.py
@@ -1,9 +1,23 @@
 import unittest
+from smallest_distinct_window import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.smallest_distinct = Solution().smallest_distinct_window
+
+    def test_case1(self):
+        self.assertEqual(self.smallest_distinct("GEEKSGEEKSFOR"), 8)  # add assertion here
+
+    def test_case2(self):
+        self.assertEqual(self.smallest_distinct("AABBBCBBAC"), 3)
+
+    def test_case3(self):
+        self.assertEqual(self.smallest_distinct("aaab"), 2)
+
+    def test_case4(self):
+        self.assertEqual(self.smallest_distinct("ACbCACAcACaB"), 10)
 
 
 if __name__ == '__main__':
