d57908a255858b97929ebf8890f6cbe47d6c4566
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-11 19:38:22 +0300

    added testcases

--- GeekforGeek/Check if Permutation is Substring/test_search_with_anagrams.py
+++ GeekforGeek/Check if Permutation is Substring/test_search_with_anagrams.py
@@ -1,9 +1,23 @@
 import unittest
+from search_with_anagrams import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.search = Solution().search
+
+    def test_case1(self):
+        self.assertEqual(self.search("geeks", "eke"), True)  # add assertion here
+
+    def test_case2(self):
+        self.assertEqual(self.search("programming", "rain"), False)
+
+    # def test_case3(self):
+    #     self.assertEqual(True, False)
+    #
+    # def test_case4(self):
+    #     self.assertEqual(True, False)
 
 
 if __name__ == '__main__':
