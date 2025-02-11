7d06dc04e1b08740442f4e9356863f62f0712100
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-11 15:30:40 +0300

    added test cases and edge cases

--- leetcode/ Best Time to Buy and Sell Stock  (121)/test_best_time_buy_sell_stock.py
+++ leetcode/ Best Time to Buy and Sell Stock  (121)/test_best_time_buy_sell_stock.py
@@ -1,9 +1,20 @@
 import unittest
+from best_time_buy_sell_stock import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.max_profit = Solution().max_profit
+
+    def test_case1(self):
+        self.assertEqual(self.max_profit([7, 1, 5, 3, 6, 4]), 5)  # add assertion here
+
+    def test_case2(self):
+        self.assertEqual(self.max_profit([7, 6, 4, 3, 1]), 0)
+
+    def test_case3(self):
+        self.assertEqual(self.max_profit([23, 54, 6, 7, 67, 78]), 72)
 
 
 if __name__ == '__main__':
