63f5c78ea594c505ad16877f2a5455917970831f
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-16 00:29:53 +0300

    added testcases with edge cases

--- leetcode/Longest Palindromic Substring  (5)/test_longest_palindrome.py
+++ leetcode/Longest Palindromic Substring  (5)/test_longest_palindrome.py
@@ -1,9 +1,35 @@
 import unittest
+from longest_palindrome import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.longest = Solution().length_palindrome
+
+    def test_case1(self):
+        self.assertIn(self.longest("babad"), ["bab", "aba"])  # add assertion here
+
+    def test_case2(self):
+        self.assertEqual(self.longest("cbbd"), "bb")
+
+    def test_case3(self):
+        self.assertEqual(self.longest("noon"), "noon")
+
+    def test_case4(self):
+        self.assertIn(self.longest("ac"), ["a", "c"])
+
+    def test_case5(self):
+        self.assertEqual(self.longest("a"), "a")
+
+    def test_case6(self):
+        self.assertEqual(self.longest("aa"), "aa")
+
+    def test_case7(self):
+        self.assertEqual(self.longest("racecar"), "racecar")
+
+    def test_case8(self):
+        self.assertEqual(self.longest("aacabdkacaa"), "aca")
 
 
 if __name__ == '__main__':
