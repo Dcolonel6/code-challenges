8ee8b2d18a5a8d1d419cbd0c9c6126dbde69a470
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-16 22:13:48 +0300

    added an new test example

--- leetcode/Longest Palindromic Substring  (5)/longest_palindrome.py
+++ leetcode/Longest Palindromic Substring  (5)/longest_palindrome.py
@@ -50,7 +50,7 @@ class Solution:
 
 if __name__ == "__main__":
     # print(Solution().length_palindrome("babad")) # "bab"
-    print(Solution().length_palindrome("ac")) # "noon"
+    print(Solution().length_palindrome("racecar")) # "noon"
 
 
 
