c243dad96b9a2c9b80deda2f6c53685ed225456b
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-18 15:29:26 +0300

    added testcases

--- codewars/Counting Duplicates/test_count_duplicate.py
+++ codewars/Counting Duplicates/test_count_duplicate.py
@@ -1,9 +1,23 @@
 import unittest
+from count_duplicate import duplicate_count
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def test_case1(self):
+        self.assertEqual(duplicate_count(""), 0)  # add assertion here
+
+    def test_case2(self):
+        self.assertEqual(duplicate_count("abcde"), 0)
+
+    def test_case3(self):
+        self.assertEqual(duplicate_count("abcdeaa"), 1)
+
+    def test_case4(self):
+        self.assertEqual(duplicate_count("abcdeaB"), 2)
+
+    def test_case5(self):
+        self.assertEqual(duplicate_count("Indivisibilities"), 2)
 
 
 if __name__ == '__main__':
