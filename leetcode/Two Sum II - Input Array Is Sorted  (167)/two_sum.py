26b5f7d0c41c8455930e3f9544f7bffb7a652f61
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-21 17:39:25 +0300

    runtime O(n) space complexity 0(1)
    inspired by binary search only difference is that we dont halve on each iteration and therefore we dont use mid point.
    Instead we use a left pointer and right pointer and move them towards each other.
    If subsum < target increase the left pointer otherwise decrease the right pointer

--- leetcode/Two Sum II - Input Array Is Sorted  (167)/two_sum.py
+++ leetcode/Two Sum II - Input Array Is Sorted  (167)/two_sum.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def two_sum(self, numbers:List[int], target: int) -> List[int]:
+    def two_sum_solution1(self, numbers:List[int], target: int) -> List[int]:
         tracker = {}
 
         for index, num in enumerate(numbers):
@@ -12,6 +12,21 @@ class Solution:
             else:
                 tracker[difference] = index
 
+    def two_sum_binary_search(self, numbers:List[int], target: int) -> int:
+        length = len(numbers)
+        left, right = 0, length - 1
+
+        while left <= right:
+            sub_sum = numbers[left] + numbers[right]
+
+            if sub_sum == target:
+                return [left + 1, right + 1]
+
+            elif sub_sum < target:
+                left += 1
+            else:
+                right -= 1
+
 
 if __name__ == "__main__":
-    print(Solution().two_sum([2, 7, 11, 15], 9))
+    print(Solution().two_sum_binary_search([2, 7, 11, 15], 9))
