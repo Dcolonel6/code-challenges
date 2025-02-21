f1039358efad68db430f435f485baaca14fca0c0
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-21 17:10:41 +0300

    added test cases

--- leetcode/Two Sum II - Input Array Is Sorted  (167)/test_two_sum.py
+++ leetcode/Two Sum II - Input Array Is Sorted  (167)/test_two_sum.py
@@ -1,9 +1,20 @@
 import unittest
+from two_sum import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.two_sum = Solution().two_sum
+
+    def test_case1(self):
+        self.assertEqual(self.two_sum([2, 7, 11, 15], 9), [1, 2])
+
+    def test_case2(self):
+        self.assertEqual(self.two_sum([2, 3, 4], 6), [1, 3])
+
+    def test_case3(self):
+        self.assertEqual(self.two_sum([-1, 0], -1), [1, 2])
 
 
 if __name__ == '__main__':
