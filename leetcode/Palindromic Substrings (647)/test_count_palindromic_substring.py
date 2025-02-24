47f83a1cb0699d14924b0b7ecbbd505616637d1a
Dennis M Njogu <dcolonel6@gmail.com>
2025-02-24 16:21:01 +0300

    Revert "added test cases with some edge cases" cause i added the solution in the commit
    This reverts commit b93df30198febdb1eb630024a07c0fc37b609a3b.

--- leetcode/Palindromic Substrings (647)/test_count_palindromic_substring.py
+++ leetcode/Palindromic Substrings (647)/test_count_palindromic_substring.py
@@ -1,32 +1,9 @@
 import unittest
-from count_palindromic_substring import Solution
 
 
 class MyTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.count_palindromes = Solution().count_substring
-
-    def test_case1(self):
-        self.assertEqual(self.count_palindromes("abc"), 3)
-
-    def test_case2(self):
-        self.assertEqual(self.count_palindromes("aaa"), 6)
-
-    def test_case3(self):
-        self.assertEqual(self.count_palindromes("aa"), 3)
-
-    def test_case4(self):
-        self.assertEqual(self.count_palindromes("aaaaa"), 15)
-
-    def test_case5(self):
-        self.assertEqual(self.count_palindromes("a"), 1)
-
-    def test_case6(self):
-        self.assertEqual(self.count_palindromes(""), 0)
-
-    def test_case7(self):
-        self.assertEqual(self.count_palindromes(" "), 1)
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
