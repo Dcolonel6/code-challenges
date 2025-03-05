bc67774219ad88bded9c880c12912e988bd27f10
Dennis M Njogu <dcolonel6@gmail.com>
2025-03-05 23:42:18 +0300

    added more tests with edge cases

--- leetcode/Maximum Product Subarray (152)/test_max_product.py
+++ leetcode/Maximum Product Subarray (152)/test_max_product.py
@@ -1,9 +1,19 @@
 import unittest
+from max_product import Solution
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self):
+        self.max_product = Solution().maximum_product
+
+    def test_case1(self):
+        self.assertEqual(self.max_product([2,-5,-2,-4,3]), 24)
+
+    def test_case2(self):
+        self.assertEqual(self.max_product([-2,0,-1]), 0)
+
+    def test_case3(self):
+        self.assertEqual(self.max_product([2,3,-2,4]), 6)
 
 
 if __name__ == '__main__':
